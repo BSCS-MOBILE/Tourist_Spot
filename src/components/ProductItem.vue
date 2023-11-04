6b11966c9d111c7d72531d40d14ba2c6e4406d4f
satos <satos@wylog.com>
2023-11-04 11:19:34 +0800

    Change link from anchor to just href

--- src/components/ProductItem.vue
+++ src/components/ProductItem.vue
@@ -1,6 +1,5 @@
 <template>
-    <ion-card :style="{ backgroundImage: 'url(' + image + ')' }" class="bg-size item-size">
-      <a :href="'/product-details/'+id">
+    <ion-card :style="{ backgroundImage: 'url(' + image + ')' }" class="bg-size item-size" :href="'/product-details/'+id">
         <ion-card-header>
           <ion-card-title class="ion-text-center">{{productName}}</ion-card-title>
           <ion-card-subtitle>{{ price }}</ion-card-subtitle>
@@ -8,17 +7,18 @@
         <ion-card-content>
           short description.
         </ion-card-content>
-      </a>
       </ion-card>
 </template>
 <script setup lang="ts">
 import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/vue';
+import { useRouter } from 'vue-router';
 defineProps({
   productName: String,
   price: String,
   image: String,
   id: Number
 });
+const router = useRouter();
 </script>
 
 
