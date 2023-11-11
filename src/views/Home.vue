d84413e1c84e8e23af80e44cdeb5a21ba9c5506c
satos <satos@wylog.com>
2023-11-11 13:19:55 +0800

    Fix text directive

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,15 +2,13 @@
   <ion-page>
     <ion-content :fullscreen="true">
       <header-layout name="Home"/>
-      <!-- <ExploreContainer name="Tab 1 page" /> -->
       <product-item v-for="n in num" :key="n.id" :product-name="n.productName" :price="n.price" :image="n.image" :id="n.id"/>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, onIonViewWillEnter, onIonViewDidEnter, onIonViewWillLeave, onIonViewDidLeave} from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import { IonPage, IonContent, onIonViewWillEnter, onIonViewDidEnter, onIonViewWillLeave, onIonViewDidLeave} from '@ionic/vue';
 import HeaderLayout from '../layout/HeaderLayout.vue'
 import ProductItem from '../components/ProductItem.vue'
 import { ref } from 'vue'
@@ -19,7 +17,8 @@ const num = ref([
   {id: 2, productName: 'Puto Cake', price: '30.00', image:'/food2.png'},
   {id: 3, productName: 'Apple Cake', price: '60.00', image:'/food.jpg'}
 ]);
-onIonViewDidEnter(() => {
+
+    onIonViewDidEnter(() => {
       console.log('Home page did enter');
     });
 
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,16 +4,17 @@
     <ion-content :fullscreen="true">
       <header-layout name="Login"/>
       <div class="container">
+        <p>{{ email }}</p>
         <ion-item>
           <ion-icon :icon="mail"></ion-icon>
-          <ion-input type="email" placeholder="Email"></ion-input>
+          <ion-input type="email" v-model="email" @input="update" placeholder="Email"></ion-input>
         </ion-item>
   
         <ion-item>
           <ion-icon :icon="key"></ion-icon>
           <ion-input type="password" placeholder="Password"></ion-input>
         </ion-item>
-        <ion-button>Login</ion-button><br/>
+        <ion-button @click="update">Login</ion-button><br/>
         <a href="/register">Register</a>
       </div>
   </ion-content>
@@ -25,6 +26,15 @@ import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonInput
 import ExploreContainer from '@/components/ExploreContainer.vue';
 import HeaderLayout from '../layout/HeaderLayout.vue'
 import { mail, key } from "ionicons/icons"
+import { ref } from 'vue'
+let title = "BSCS4";
+let htmlContent = "<h1>Hello BSCS4</h1>";
+let email = ref('');
+let isWorld = false;
+function update() {
+  console.log(email)
+  
+}
 </script>
 
 <style scoped>
